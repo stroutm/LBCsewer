9d450c288b4b35d260e8ab9453bf8ddd4c343655
stroutm <stroutm@umich.edu>
2018-05-09 16:58:11 -0400

    fixed deep copy for repTot proceedure

--- code/test_environment_mbc_GDRSS.py
+++ code/test_environment_mbc_GDRSS.py
@@ -3,58 +3,72 @@ from mbc_fn import mbc, mbc_bin, perf, TSScalc
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import copy
 
 # Weighting parameters
 beta = 1.0 # upstream depth
-epsilons = [100.0,10.0] # downstream flow
+epsilons = [100.0] # downstream flow
 gammas = [0.0] # downstream flow derivative
 zetas = [0.0] # downstream TSS loading
 
 # Downstream setpoints
-setptFlow = 0.3 # normalized to max_depth
+setptFlow = 0.2 # normalized to max_depth
 setptFlowDeriv = 0.0
 setptTSSload = 0.0
 
 # Control specifications
-repTot = 1 # only control every repTot steps
-contType = "continuous" # "binary" {0,1} or "continuous" [0,1] gate openings
+repTot = 90 # only control every repTot steps
+contType = "binary" # "binary" {0,1} or "continuous" [0,1] gate openings
 deriv = 0 # 1 to control for flow derivative; 0 otherwise
 # TSS not yet established for GDRSS
 TSS = 0 # 1 to control for TSS; 0 otherwise
 
 # Plotting/saving specifications
-save = 1 # 1 to save results; 0 otherwise
-saveNames = ['iter04','iter04_a','iter04_b']
-    # first should be no control simulation name;
+save = 0 # 1 to save results; 0 otherwise
+saveNames = ['enter']
+    # first should be no control simulation name (if doing no control simulation);
     # all others should be for control simulations
 plot = 1 # 1 to plot results; 0 otherwise
 linestyles = ['-','-.'] # linestyles for different parameter trials
-colors = ['#00a650','#008bff','#ff4a00','#ffb502','#9200cc']
+colors = ['#00a650','#008bff','#ff4a00','#ffb502','#9200cc','#b4e300','#ff03a0','#00e3ce','#0011ab','#a40000']
         # colors for each upstream asset
-ustream_labels = ['ISD006','ISD007','ISD008','ISD009','ISD010']
+#ustream_labels = ['ISD002','ISD003','ISD004']
+#ustream_labels = ['ISD006','ISD007','ISD008','ISD009','ISD010']
+ustream_labels = ['ISD011','ISD012','ISD013']
 noControl = 1 # 1 to include no control simulation; 0 otherwise
 
 # States to pull from simulation (must include upstream and downstream states
 # for control objectives)
+discharge = 1.0 # discharge coefficient of orifices
 state_space = {"depthsN":[],
-                "depthsL":["2150","2180","2190","2198","2220"],
-                "flows":["21450"],
+                #"depthsL":["1509","RC1951","1535"],
+                #"depthsL":["2160","21853","21950","2201","2388"],
+                "depthsL":["2765","2525","2360"],
+                #"flows":["1503"],
+                #"flows":["21450"],
+                "flows":["27450"],
                 "inflows":[]}
 n_tanks = len(state_space['depthsL']) # number of upstream tanks/conduits
-control_points = ["ISD006_DOWN","ISD007_DOWN","ISD008_DOWN","ISD009_DOWN","ISD010_DOWN",
-                    "ISD006_UP","ISD007_UP","ISD008_UP","ISD009_UP","ISD010_UP"]
-max_depths = [15.5,15.5,15.5,15.5,12.25] # upstream tanks/conduits max depths for
+#control_points = ["ISD002_DOWN","ISD003_DOWN","ISD004_DOWN",
+#                    "ISD002_UP","ISD003_UP","ISD004_UP"]
+#control_points = ["ISD006_DOWN","ISD007_DOWN","ISD008_DOWN","ISD009_DOWN","ISD010_DOWN",
+#                    "ISD006_UP","ISD007_UP","ISD008_UP","ISD009_UP","ISD010_UP"]
+control_points = ["ISD011_DOWN","ISD012_DOWN","ISD013_DOWN",
+                    "ISD011_UP","ISD012_UP","ISD013_UP"]
+#max_depths = [13.45,20.0,14.0]
+#max_depths = [15.5,15.5,15.5,15.5,12.25] # upstream tanks/conduits max depths for
                             # normalization
-max_flow = 158 # downstream max flow for normalization;
-                # 585 peak flow for no control for conduit 1503;
-                # 1193.0455 as calculated for conduit 1503
+max_depths = [15.5,10.5,11.5]
+#max_flow = 412
+#max_flow = 247 # downstream max flow for normalization
+max_flow = 373
 routime_step = 10 # routing timestep in seconds
 
 # Input file, state space, and control points
 #env = Env("../data/input_files/GDRSS/pyswmm_GDRSS_withISDs_RainEvents_1.inp",
 #    state_space,
 #    control_points)
-env = Env("../data/input_files/GDRSS/GDRSS_simple3_ISD_Rework_GJE_edit.inp",
+env = Env("../data/input_files/GDRSS/GDRSS_simple3_ISD_Rework_GJE_edit_SCT_delete234.inp",
     state_space,
     control_points)
 
@@ -66,7 +80,7 @@ if noControl == 1:
     done = False; j = 0
     while not done:
         j += 1
-        state, done = env.step(np.hstack((np.ones(n_tanks),np.zeros(n_tanks))))
+        state, done = env.step(np.hstack((np.ones(n_tanks),np.zeros(n_tanks+1))))
         if j == 1:
             ustream_depths = state[0][0:n_tanks]/max_depths
             dstream_flow = state[0][n_tanks]/max_flow
@@ -106,11 +120,11 @@ if noControl == 1:
 
     print('Done with no control')
 
-if save == 1:
-    fileName = '../data/results/no_control/' + saveNames[0] + '.pkl'
-    with open(fileName,'w') as f:
-        pickle.dump([state_space,ustream_depths,dstream_flow],f)
-    print('No control results saved')
+    if save == 1:
+        fileName = '../data/results/no_control/' + saveNames[0] + '.pkl'
+        with open(fileName,'w') as f:
+            pickle.dump([state_space,ustream_depths,dstream_flow],f)
+        print('No control results saved')
 
 k = -1
 for epsilon in epsilons:
@@ -168,18 +182,18 @@ for epsilon in epsilons:
                     setpts = np.array([setptFlow, setptFlowDeriv, setptTSSload])
                     dparam = np.array([epsilon, gamma, zeta])
                 uparam = beta
-                actPrev = action[:]
+                actPrev = copy.deepcopy(action)
                 if contType == "binary":
                     p, PD, PS, action = mbc_bin(ustream, dstream, setpts, uparam,
-                                                dparam, n_tanks, action)
+                                                dparam, n_tanks, action, discharge)
                 elif contType == "continuous":
                     p, PD, PS, action = mbc(ustream, dstream, setpts, uparam,
-                                            dparam, n_tanks, action)
-                actCurr = action[:]
+                                            dparam, n_tanks, action, discharge, max_flow)
+                actCurr = copy.deepcopy(action)
                 if repTot > 1:
-                    action = actPrev[:]
+                    action = copy.deepcopy(actPrev)
                 else:
-                    action = actCurr[:]
+                    action = copy.deepcopy(actCurr)
 
                 if j == 1:
                     ustream_depths = state[0][0:n_tanks]/max_depths
@@ -204,9 +218,7 @@ for epsilon in epsilons:
                     rep = 0
                     while rep < repTot:
                         rep += 1
-                        print('rep')
                         if done == False:
-                            # include something to make sure gate opens if upstream > 0.95
                             state, done = env.step(action)
                             for m in range(0,n_tanks):
                                 action[m] = gateTrans[m,rep]
@@ -214,12 +226,14 @@ for epsilon in epsilons:
                             if j == 1:
                                 ustream_depths = state[0][0:n_tanks]/max_depths
                                 dstream_flow = state[0][n_tanks]/max_flow
+                                price = p
                                 demands = PD
                                 supply = PS
                                 gates = action
                             else:
                                 ustream_depths = np.vstack((ustream_depths,state[0][0:n_tanks]/max_depths))
                                 dstream_flow = np.vstack((dstream_flow,state[0][n_tanks]/max_flow))
+                                price = np.vstack((price,p))
                                 demands = np.vstack((demands,PD))
                                 supply = np.vstack((supply,PS))
                                 gates = np.vstack((gates,action))
@@ -271,7 +285,10 @@ for epsilon in epsilons:
                     ', gamma = ' + str(gamma) + ', zeta = ' + str(zeta))
 
             if save == 1:
-                fileName = '../data/results/control/' + saveNames[k+1] + '.pkl'
+                if noControl == 1:
+                    fileName = '../data/results/control/' + saveNames[k+1] + '.pkl'
+                else:
+                    fileName = '../data/results/control/' + saveNames[k] + '.pkl'
                 with open(fileName,'w') as f:
                     pickle.dump([beta,epsilon,gamma,zeta,setptFlow,setptFlowDeriv,
                                 setptTSSload,repTot,contType,state_space,control_points,
