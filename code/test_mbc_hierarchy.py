c8256097adb48370783f2c42e4cf50a534888aec
stroutm <stroutm@umich.edu>
2018-06-05 12:32:35 -0400

    minor plotting updates (e.g., colors, labels)

--- code/test_mbc_hierarchy.py
+++ code/test_mbc_hierarchy.py
@@ -11,13 +11,13 @@ n_ISDs = [3,5,3] # array with number of ISD along each main trunkline branch
 
 # Weighting parameters
 beta = 1.0 # upstream flow
-epsilon = 100.0 # downstream flow
+epsilon = 10.0 # downstream flow
 
 # Downstream setpoints
-setptMethod = "automatic" # "manual" to use setpts array established below;
+setptMethod = "manual" # "manual" to use setpts array established below;
                 #"automatic" to determine setpoints for each branch using MBC and setpt_WRRF
-setptThres = 1 # 1 if not to exceed setpt; 0 if to achieve setpt
-setpts = [0.4,0.4,0.4] # same order as branches in n_ISDs
+setptThres = 0 # 1 if not to exceed setpt; 0 if to achieve setpt
+setpts = [0.4,0.5,0.3] # same order as branches in n_ISDs
 setpt_WRRF = 0.4 # setpoint for flow at WRRF
 
 # Control specifications
@@ -25,8 +25,11 @@ contType = "continuous" # "binary" {0,1} or "continuous" [0,1] gate openings
 save = 0 # 1 to save results; 0 otherwise
 saveNames = ['check']
 plot = 1 # 1 to plot results; 0 otherwise
-colors = ['#00a650','#008bff','#ff4a00','#ffb502','#9200cc','#b4e300','#ff03a0','#00e3ce','#0011ab','#a40000','#996f3d']
-noControl = 0 # 1 to include no control simulation; 0 otherwise
+normalize = 0 # 1 to normalize downstream flows for plotting; 0 otherwise
+#colors = ['#00a650','#008bff','#ff4a00','#ffb502','#9200cc','#b4e300','#ff03a0','#00e3ce','#0011ab','#a40000','#996f3d']
+colors = []
+labels = []
+noControl = 1 # 1 to include no control simulation; 0 otherwise
 control = 1 # 1 to include control simulation; 0 otherwise
 
 units = "english" # "english" for English units in .inp file; "metric" otherwise
@@ -45,27 +48,27 @@ dInvert = []
 orifice_diam_all = []
 
 if 13 in ISDs:
-    state_space["depthsN"].append("2355"); state_space["depthsL"].append("2360"); control_points.append("ISD013_DOWN"); max_depths.append(11.5); uInvert.append(604.76); dInvert.append(604.71); orifice_diam_all.append(11.5)
+    state_space["depthsN"].append("2355"); state_space["depthsL"].append("2360"); control_points.append("ISD013_DOWN"); max_depths.append(11.5); uInvert.append(604.76); dInvert.append(604.71); orifice_diam_all.append(11.5); colors.append('#276278'); labels.append('K')
 if 12 in ISDs:
-    state_space["depthsN"].append("25250"); state_space["depthsL"].append("2525"); control_points.append("ISD012_DOWN"); max_depths.append(10.5); uInvert.append(604.56); dInvert.append(604.51); orifice_diam_all.append(10.5)
+    state_space["depthsN"].append("25250"); state_space["depthsL"].append("2525"); control_points.append("ISD012_DOWN"); max_depths.append(10.5); uInvert.append(604.56); dInvert.append(604.51); orifice_diam_all.append(10.5); colors.append('#167070'); labels.append('J')
 if 11 in ISDs:
-    state_space["depthsN"].append("2745"); state_space["depthsL"].append("2765"); control_points.append("ISD011_DOWN"); max_depths.append(15.5); uInvert.append(580.86); dInvert.append(580.81); orifice_diam_all.append(15.5)
+    state_space["depthsN"].append("2745"); state_space["depthsL"].append("2765"); control_points.append("ISD011_DOWN"); max_depths.append(15.5); uInvert.append(580.86); dInvert.append(580.81); orifice_diam_all.append(15.5); colors.append('#55a6a6'); labels.append('I')
 if 10 in ISDs:
-    state_space["depthsN"].append("22201"); state_space["depthsL"].append("2388"); control_points.append("ISD010_DOWN"); max_depths.append(12.25); uInvert.append(603.86); dInvert.append(603.81); orifice_diam_all.append(12.25)
+    state_space["depthsN"].append("22201"); state_space["depthsL"].append("2388"); control_points.append("ISD010_DOWN"); max_depths.append(12.25); uInvert.append(603.86); dInvert.append(603.81); orifice_diam_all.append(12.25); colors.append('#b5491a'); labels.append('H')
 if 9 in ISDs:
-    state_space["depthsN"].append("2195"); state_space["depthsL"].append("2201"); control_points.append("ISD009_DOWN"); max_depths.append(15.5); uInvert.append(591.36); dInvert.append(591.31); orifice_diam_all.append(15.5)
+    state_space["depthsN"].append("2195"); state_space["depthsL"].append("2201"); control_points.append("ISD009_DOWN"); max_depths.append(15.5); uInvert.append(591.36); dInvert.append(591.31); orifice_diam_all.append(15.5); colors.append('#fe6625'); labels.append('G')
 if 8 in ISDs:
-    state_space["depthsN"].append("2185"); state_space["depthsL"].append("21950"); control_points.append("ISD008_DOWN"); max_depths.append(15.5); uInvert.append(585.26); dInvert.append(585.21); orifice_diam_all.append(15.5)
+    state_space["depthsN"].append("2185"); state_space["depthsL"].append("21950"); control_points.append("ISD008_DOWN"); max_depths.append(15.5); uInvert.append(585.26); dInvert.append(585.21); orifice_diam_all.append(15.5); colors.append('#fe9262'); labels.append('F')
 if 7 in ISDs:
-    state_space["depthsN"].append("2170"); state_space["depthsL"].append("21853"); control_points.append("ISD007_DOWN"); max_depths.append(15.5); uInvert.append(578.86); dInvert.append(578.81); orifice_diam_all.append(15.5)
+    state_space["depthsN"].append("2170"); state_space["depthsL"].append("21853"); control_points.append("ISD007_DOWN"); max_depths.append(15.5); uInvert.append(578.86); dInvert.append(578.81); orifice_diam_all.append(15.5); colors.append('#fb9334'); labels.append('E')
 if 6 in ISDs:
-    state_space["depthsN"].append("2145"); state_space["depthsL"].append("2160"); control_points.append("ISD006_DOWN"); max_depths.append(15.5); uInvert.append(572.46); dInvert.append(572.46); orifice_diam_all.append(15.5)
+    state_space["depthsN"].append("2145"); state_space["depthsL"].append("2160"); control_points.append("ISD006_DOWN"); max_depths.append(15.5); uInvert.append(572.46); dInvert.append(572.46); orifice_diam_all.append(15.5); colors.append('#ffb16c'); labels.append('D')
 if 4 in ISDs:
-    state_space["depthsN"].append("1516"); state_space["depthsL"].append("1535"); control_points.append("ISD004_DOWN"); max_depths.append(14.0); uInvert.append(587.36); dInvert.append(587.31); orifice_diam_all.append(14.0)
+    state_space["depthsN"].append("1516"); state_space["depthsL"].append("1535"); control_points.append("ISD004_DOWN"); max_depths.append(14.0); uInvert.append(587.36); dInvert.append(587.31); orifice_diam_all.append(14.0); colors.append('#414a4f'); labels.append('C')
 if 3 in ISDs:
-    state_space["depthsN"].append("1952"); state_space["depthsL"].append("RC1951"); control_points.append("ISD003_DOWN"); max_depths.append(20.0); uInvert.append(586.36); dInvert.append(586.31); orifice_diam_all.append(9.0)
+    state_space["depthsN"].append("1952"); state_space["depthsL"].append("RC1951"); control_points.append("ISD003_DOWN"); max_depths.append(20.0); uInvert.append(586.36); dInvert.append(586.31); orifice_diam_all.append(9.0); colors.append('#006592'); labels.append('B')
 if 2 in ISDs:
-    state_space["depthsN"].append("1504"); state_space["depthsL"].append("1509"); control_points.append("ISD002_DOWN"); max_depths.append(13.45); uInvert.append(581.66); dInvert.append(581.66); orifice_diam_all.append(14.7)
+    state_space["depthsN"].append("1504"); state_space["depthsL"].append("1509"); control_points.append("ISD002_DOWN"); max_depths.append(13.45); uInvert.append(581.66); dInvert.append(581.66); orifice_diam_all.append(14.7); colors.append('#003d59'); labels.append('A')
 
 if 13 in ISDs:
     state_space["depthsN"].append("23554"); control_points.append("ISD013_UP")
@@ -91,11 +94,13 @@ if 2 in ISDs:
     state_space["depthsN"].append("15031"); control_points.append("ISD002_UP")
 
 if (13 in ISDs) or (12 in ISDs) or (11 in ISDs):
-    state_space["flows"].append("27450")
+    state_space["flows"].append("27450"); colors.append('#167070'); labels.append('I-K')
 if (10 in ISDs) or (9 in ISDs) or (8 in ISDs) or (7 in ISDs) or (6 in ISDs):
-    state_space["flows"].append("21450")
+    state_space["flows"].append("21450"); colors.append('#fe6625'); labels.append('D-H')
 if (4 in ISDs) or (3 in ISDs) or (2 in ISDs):
-    state_space["flows"].append("1503")
+    state_space["flows"].append("1503"); colors.append('#003d59'); labels.append('A-C')
+
+colors.append('#66747c')
 
 # Input file, state space, and control points
 env = Env("../data/input_files/GDRSS/GDRSS_simple3_ISD_Rework_GJE_edit_SCT_delete234.inp",
@@ -117,27 +122,29 @@ if noControl == 1:
             dstream_flows = np.vstack((dstream_flows,state[0][-1*n_trunkline:]))
 
     max_flow_WRRF = max(WRRF_flow)
-    WRRF_flow = WRRF_flow/max_flow_WRRF
+    if normalize == 1:
+        WRRF_flow = WRRF_flow/max_flow_WRRF
     for q in range(0,n_trunkline):
         max_flow_dstream[q] = max(dstream_flows[:,q])
-        dstream_flows[:,q] = dstream_flows[:,q]/max_flow_dstream[q]
+        if normalize == 1:
+            dstream_flows[:,q] = dstream_flows[:,q]/max_flow_dstream[q]
 
     if plot == 1:
         plt.subplot(321)
         for a in range(0,sum(n_ISDs)):
             plt.plot(ustream_depths[:,a],
-                        label = "No control, " + control_points[a],
-                        color = colors[a], linestyle = '--')
+                        label = "No control, " + labels[a],
+                        color = colors[a], linestyle = '-')
 
         plt.subplot(322)
-        plt.plot(WRRF_flow, label = "No control, WRRF flow", color = colors[0],
-                    linestyle = '--')
+        plt.plot(WRRF_flow, label = "No control, WRRF flow", color = colors[-1],
+                    linestyle = '-')
 
         plt.subplot(324)
         for a in range(0,n_trunkline):
             plt.plot(dstream_flows[:,a],
-                        label = "No control, " + state_space['flows'][a+1],
-                        color = colors[a], linestyle = '--')
+                        label = "No control, " + labels[a-n_trunkline],
+                        color = colors[a-n_trunkline-1], linestyle = '-')
 
     print("Done with no control")
 
@@ -154,7 +161,10 @@ if control == 1:
         ps = np.zeros(n_trunkline)
 
         if setptMethod == "automatic":
-            ustream = state[0,-n_trunkline:]/max_flow_dstream # dstream flows at branch/trunkline
+            #ustream = state[0,-n_trunkline:]/max_flow_dstream # dstream flows at branch/trunkline
+            ustream = np.zeros(n_trunkline) # max fullness of storage along branches
+            for b in range(0,n_trunkline):
+                ustream[b] = max(state[0,2*sum(n_ISDs)+sum(n_ISDs[0:b]):2*sum(n_ISDs)+sum(n_ISDs[0:b+1])]/max_depths[sum(n_ISDs[0:b]):sum(n_ISDs[0:b+1])])
             dstream = np.array([state[0,-n_trunkline-1]/max_flow_WRRF]) # WRRF flow
             setpt = np.array([setpt_WRRF])
             uparam = beta
@@ -219,60 +229,84 @@ if control == 1:
         for a in range(0,sum(n_ISDs)):
             plt.subplot(321)
             plt.plot(ustream_depths[:,a],
-                        label = "MBC, " + control_points[a],
+                        label = "MBC, " + labels[a],
                         color = colors[a], linestyle = '-')
 
             plt.subplot(325)
             plt.plot(gates[:,a],
-                        label = "Dam down, " + control_points[a],
+                        label = "Dam down, " + labels[a],
                         color = colors[a], linestyle = '-')
 
             plt.subplot(326)
             plt.plot(demands[:,a],
-                        label = "Demand, " + control_points[a],
-                        color = colors[a], linestyle = '-')
+                        label = "Demand, " + labels[a],
+                        color = colors[a-n_trunkline-1], linestyle = '-')
 
         plt.subplot(322)
-        plt.plot(WRRF_flow, label = "MBC, WRRF flow", color = colors[0],
+        if normalize == 1:
+            plt.plot(WRRF_flow, label = "MBC, WRRF flow", color = colors[-1],
                     linestyle = '-')
+            if setptMethod == "automatic":
+                plt.plot(setpt_WRRF*np.ones(len(WRRF_flow)), color = 'k', label = 'Setpoint')
+        else:
+            plt.plot(max_flow_WRRF*WRRF_flow, label = "MBC, WRRF flow", color = colors[-1],
+                    linestyle = '-')
+            if setptMethod == "automatic":
+                plt.plot(max_flow_WRRF*setpt_WRRF*np.ones(len(WRRF_flow)), color = 'k', label = 'Setpoint')
 
         for a in range(0,n_trunkline):
             plt.subplot(324)
-            plt.plot(dstream_flows[:,a],
-                        label = "MBC, " + state_space['flows'][a+1],
-                        color = colors[a], linestyle = '-')
-            plt.plot(setpts_all[:,a],
-                        label = "Setpoint, " + state_space['flows'][a+1],
-                        color = colors[a], linestyle = ':')
+            if normalize == 1:
+                plt.plot(dstream_flows[:,a],
+                        label = "MBC, " + labels[a-n_trunkline],
+                        color = colors[a-n_trunkline-1], linestyle = '-')
+                plt.plot(setpts_all[:,a],
+                        label = "Setpoint, " + labels[a-n_trunkline],
+                        color = colors[a-n_trunkline-1], linestyle = ':')
+            else:
+                plt.plot(max_flow_dstream[a]*dstream_flows[:,a],
+                        label = "MBC, " + labels[a-n_trunkline],
+                        color = colors[a-n_trunkline-1], linestyle = '-')
+                plt.plot(max_flow_dstream[a]*setpts_all[:,a],
+                        label = "Setpoint, " + labels[a-n_trunkline],
+                        color = colors[a-n_trunkline-1], linestyle = ':')
 
             plt.subplot(326)
             plt.plot(price[:,a],
-                        label = "price, " + state_space['flows'][a+1],
-                        color = colors[a], linestyle = '--')
+                        label = "price, " + labels[a-n_trunkline],
+                        color = colors[a-n_trunkline-1], linestyle = ':')
 
     print("Done with MBC")
 
 if plot == 1:
     plt.subplot(321)
     plt.ylabel('Upstream Normalized Conduit Depth')
-    plt.legend()
+    plt.ylim(0,1)
+    #plt.legend()
 
     plt.subplot(322)
-    if setptMethod == "automatic":
-        plt.plot(setpt_WRRF*np.ones(len(WRRF_flow)), color = 'k', label = 'Setpoint')
-    plt.ylabel('WRRF Normalized Flow')
-    plt.legend()
+    if normalize == 1:
+        plt.ylim(0,1)
+    else:
+        plt.ylim(0,600)
+    plt.ylabel('WRRF Flow ($\mathregular{ft^3/s}$)')
+    #plt.legend()
 
     plt.subplot(324)
-    plt.ylabel('Normalized Trunkline Flows')
-    plt.legend()
+    plt.ylabel('Branch Flows ($\mathregular{ft^3/s}$)')
+    if normalize == 1:
+        plt.ylim(0,1)
+    else:
+        plt.ylim(0,300)
+    #plt.legend()
 
     plt.subplot(325)
     plt.ylabel('Gate Opening')
-    plt.legend()
+    plt.ylim(0,1)
+    #plt.legend()
 
     plt.subplot(326)
     plt.ylabel('Price and Demands')
-    plt.legend()
+    #plt.legend()
 
     plt.show()
