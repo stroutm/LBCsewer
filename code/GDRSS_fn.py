1516a06b01049cd73670e65cc842a9531e73818d
stroutm <stroutm@umich.edu>
2018-06-25 15:49:04 -0400

    got rid of unused inflow nodes

--- code/GDRSS_fn.py
+++ code/GDRSS_fn.py
@@ -13,7 +13,6 @@ def GDRSS_build(ISDs):
     if 13 in ISDs:
         state_space["depthsN"].append("2355");
         state_space["depthsL"].append("2360");
-        state_space["inflows"].append("RC12355");
         control_points.append("ISD013_DOWN");
         max_depths.append(11.5);
         uInvert.append(604.76);
@@ -24,9 +23,6 @@ def GDRSS_build(ISDs):
     if 12 in ISDs:
         state_space["depthsN"].append("25250");
         state_space["depthsL"].append("2525");
-        state_space["inflows"].append("RC12521");
-        state_space["inflows"].append("RC12520");
-        state_space["inflows"].append("RC12770");
         control_points.append("ISD012_DOWN");
         max_depths.append(10.5);
         uInvert.append(604.56);
@@ -37,10 +33,6 @@ def GDRSS_build(ISDs):
     if 11 in ISDs:
         state_space["depthsN"].append("2745");
         state_space["depthsL"].append("2765");
-        state_space["inflows"].append("RC12350");
-        state_space["inflows"].append("RC12345");
-        state_space["inflows"].append("RC12315");
-        state_space["inflows"].append("RC12765");
         control_points.append("ISD011_DOWN");
         max_depths.append(15.5);
         uInvert.append(580.86);
@@ -51,8 +43,6 @@ def GDRSS_build(ISDs):
     if 10 in ISDs:
         state_space["depthsN"].append("22201");
         state_space["depthsL"].append("2388");
-        state_space["inflows"].append("RC12397");
-        state_space["inflows"].append("RC12389");
         control_points.append("ISD010_DOWN");
         max_depths.append(12.25);
         uInvert.append(603.86);
@@ -63,8 +53,6 @@ def GDRSS_build(ISDs):
     if 9 in ISDs:
         state_space["depthsN"].append("2195");
         state_space["depthsL"].append("2201");
-        state_space["inflows"].append("RC12201");
-        state_space["inflows"].append("RC12200");
         control_points.append("ISD009_DOWN");
         max_depths.append(15.5);
         uInvert.append(591.36);
@@ -95,8 +83,6 @@ def GDRSS_build(ISDs):
     if 6 in ISDs:
         state_space["depthsN"].append("2145");
         state_space["depthsL"].append("2160");
-        state_space["inflows"].append("RC32160");
-        state_space["inflows"].append("RC3214500");
         control_points.append("ISD006_DOWN");
         max_depths.append(15.5);
         uInvert.append(572.46);
@@ -107,11 +93,6 @@ def GDRSS_build(ISDs):
     if 4 in ISDs:
         state_space["depthsN"].append("1516");
         state_space["depthsL"].append("1535");
-        state_space["inflows"].append("RC11570");
-        state_space["inflows"].append("RC11571");
-        state_space["inflows"].append("RC11550");
-        state_space["inflows"].append("RC11545");
-        state_space["inflows"].append("RC11535");
         control_points.append("ISD004_DOWN");
         max_depths.append(14.0);
         uInvert.append(587.36);
@@ -122,7 +103,6 @@ def GDRSS_build(ISDs):
     if 3 in ISDs:
         state_space["depthsN"].append("1952");
         state_space["depthsL"].append("RC1951");
-        state_space["inflows"].append("RC1951");
         control_points.append("ISD003_DOWN");
         max_depths.append(20.0);
         uInvert.append(586.36);
@@ -133,7 +113,6 @@ def GDRSS_build(ISDs):
     if 2 in ISDs:
         state_space["depthsN"].append("1504");
         state_space["depthsL"].append("1509");
-        state_space["inflows"].append("RC19523");
         control_points.append("ISD002_DOWN");
         max_depths.append(13.45);
         uInvert.append(581.66);
@@ -145,47 +124,36 @@ def GDRSS_build(ISDs):
     if 13 in ISDs:
         state_space["depthsN"].append("23554");
         control_points.append("ISD013_UP");
-        state_space["inflows"].append("2351");
     if 12 in ISDs:
         state_space["depthsN"].append("25254");
         control_points.append("ISD012_UP");
-        state_space["inflows"].append("25251");
     if 11 in ISDs:
         state_space["depthsN"].append("27453");
         control_points.append("ISD011_UP");
-        state_space["inflows"].append("27450");
     if 10 in ISDs:
         state_space["depthsN"].append("22204");
         control_points.append("ISD010_UP");
-        state_space["inflows"].append("22202");
     if 9 in ISDs:
         state_space["depthsN"].append("21954");
         control_points.append("ISD009_UP");
-        state_space["inflows"].append("21950");
     if 8 in ISDs:
         state_space["depthsN"].append("21859");
         control_points.append("ISD008_UP");
-        state_space["inflows"].append("21852");
     if 7 in ISDs:
         state_space["depthsN"].append("21703");
         control_points.append("ISD007_UP");
-        state_space["inflows"].append("21700");
     if 6 in ISDs:
         state_space["depthsN"].append("21454");
         control_points.append("ISD006_UP");
-        state_space["inflows"].append("21450");
     if 4 in ISDs:
         state_space["depthsN"].append("15164");
         control_points.append("ISD004_UP");
-        state_space["inflows"].append("1515");
     if 3 in ISDs:
         state_space["depthsN"].append("19523");
         control_points.append("ISD003_UP");
-        state_space["inflows"].append("19520");
     if 2 in ISDs:
         state_space["depthsN"].append("15031");
         control_points.append("ISD002_UP");
-        state_space["inflows"].append("1503");
 
     if (13 in ISDs) or (12 in ISDs) or (11 in ISDs):
         state_space["flows"].append("27450");
@@ -198,7 +166,7 @@ def GDRSS_build(ISDs):
     if (4 in ISDs) or (3 in ISDs) or (2 in ISDs):
         state_space["flows"].append("1503");
         colors.append('#003d59');
-        labels.append('A-C'); 
+        labels.append('A-C');
 
     colors.append('#66747c')
 
