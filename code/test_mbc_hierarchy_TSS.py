d4f0500122eb5ac2b08fc393631608d8d0177bf7
stroutm <stroutm@umich.edu>
2018-10-22 09:05:05 -0400

    updated results saving

--- code/test_mbc_hierarchy_TSS.py
+++ code/test_mbc_hierarchy_TSS.py
@@ -4,6 +4,7 @@ from plot_fn import plot_noControl, plot_control, plot_finish
 from GDRSS_fn import GDRSS_build
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
 import swmmAPI as swmm
 
 # Enter 1 to include no control simulation and 0 otherwise
@@ -13,13 +14,13 @@ control = 1
 
 ## Simulation parameters
 # Enter ISDs to use for MBC; elements should be from {2,3,4}U{6,...,13}
-ISDs = [11,6,2]
-#ISDs = [13,12,11,10,9,8,7,6,4,3,2]
+#ISDs = [11,6,2]; n_ISDs = [1,1,1]
+#ISDs = [13,12,6,4,3]; n_ISDs = [2,1,2]
+ISDs = [13,12,11,10,9,8,7,6,4,3,2]; n_ISDs = [3,5,3]
 # Enter number of main trunkline branches
 n_trunkline = 3
 # Enter array with number of ISDs along each main trunkline branch, from most
 # upstream branch to most downstream
-n_ISDs = [1,1,1]
 
 ## Weighting parameters
 # Enter weighting parameters:
@@ -30,9 +31,12 @@ weights = {'beta': 1.0,
     # epsilon_TSS: downstream WRRF TSS objective
     'epsilon_TSS': 1.0
     }
-saveNames = ['trial_flow_20170504_H_A']
+saveNames = ['trial_both_201701_NH_A_more2']
+saveType = "numpy" # or "pickle"
 #eps_flows = [0.1,0.5,1.,2.,5.,7.,10.,20.,50.,100.]
-eps_flows = [100.0]
+eps_flows = [0.1,1.,10.,100.]
+#eps_TSS = [0.1,0.5,1.,2.,5.,7.,10.,20.,50.,100.]
+eps_TSS = [0.5,2.,5.,7.,20.,50.]
 
 ## Downstream setpoints
     # For setptThres, enter 1 to not exceed setpoint or 0 to achieve setpoint
@@ -40,11 +44,11 @@ ctrlParams = {'setptThres': 0,
     # For objType, enter 'flow', 'TSS', or 'both' for downstream objective type;
     # 'both' considers both objectives simulataneously, weighting based on
     # values for epsilon_flow and epsilon_TSS provided above
-    'objType': 'flow',
+    'objType': 'both',
     # For setpt_WRRF_flow and setpt_WRRF_TSS, enter downstram flow and TSS
     # setpoints, respectively, normalized to no control simulation results
-    'setpt_WRRF_flow': 0.36,
-    'setpt_WRRF_TSS': 0.493,
+    'setpt_WRRF_flow': 0.1376, #0.136
+    'setpt_WRRF_TSS': 0.4444, #0.2458
     # For contType, enter 'binary' for {0,1} gate openings or 'continuous' for [0,1]
     'contType': 'continuous',
     # For hierarchy, enter 1 to have separate markets in each branch or 0 to
@@ -61,7 +65,8 @@ headers = ['[TITLE]','[OPTIONS]','[EVAPORATION]','[RAINGAGES]','[SUBCATCHMENTS]'
     '[TIMESERIES]','[PATTERNS]','[REPORT]','[TAGS]','[MAP]','[COORDINATES]',
     '[VERTICES]','[Polygons]','[SYMBOLS]','[PROFILES]']
 # Enter .inp file name and location
-inpF = "../data/input_files/GDRSS/GDRSS_SCT_simple_ISDs_TSS_inflows.inp"
+inpF = "../data/input_files/GDRSS/GDRSS_SCT_simple_ISDs_TSS_inflows_201701.inp"
+timesteps = 3162240 #259319 #34562
 # Sections and dictionaries generated for SWMM attributes;
 # used for pulling parameters and states
 sections = swmm.make_sections(inpF,headers)
@@ -121,13 +126,13 @@ wetWeather = 1
 # if noControl == 1
 if wetWeather == 1:
     # For max_flow_WRRF, enter no control simulation WRRF flow peak
-    maxes = {'max_flow_WRRF': 619.824290,
+    maxes = {'max_flow_WRRF': 3254.,
     # For max_flow_dstream, enter no control simulaiton flow peaks for each branch
-    'max_flow_dstream': [155.621567,176.707798,332.395165],
+    'max_flow_dstream': [1152.,1295.,2798.],
     # For max_TSSLoad_WRRF, enter no control simulation WRRF TSS load peak
-    'max_TSSLoad_WRRF': 2.504154,
+    'max_TSSLoad_WRRF': 10.9661,
     # For max_TSSLoad_dstream, enter no control simulation TSS load peaks for each branch
-    'max_TSSLoad_dstream': [0.850934,0.635828,1.204579]
+    'max_TSSLoad_dstream': [5.125,3.6348,6.3125]
     }
 else:
     maxes = {'max_flow_WRRF': 223.275,
@@ -138,11 +143,11 @@ else:
 
 ## Saving and plotting specifications
 # Enter 1 to save results and 0 otherwise
-save = 0
+save = 1
 # Enter filename to save under if save == 1
 
 # For plot, enter 1 to plot results and 0 otherwise
-plotParams = {'plot': 1,
+plotParams = {'plot': 0,
     # For colors, provide array of hex colors for each ISD
     'colors': colors,
     # For labels, provide array of strings for each ISD
@@ -154,7 +159,7 @@ plotParams = {'plot': 1,
 ## No control simulation
 if noControl == 1:
     # Runs simulation for no control case
-    time, ustream_depths, dstream_flows, max_flow_dstream, dstream_TSSLoad, max_TSSLoad_dstream, WRRF_flow, max_flow_WRRF, WRRF_TSSLoad, max_TSSLoad_WRRF = simulation_noControl(env, n_trunkline, sysSpecs)
+    time, ustream_depths, dstream_flows, max_flow_dstream, dstream_TSSLoad, max_TSSLoad_dstream, WRRF_flow, max_flow_WRRF, WRRF_TSSLoad, max_TSSLoad_WRRF = simulation_noControl(env, n_trunkline, sysSpecs, timesteps)
 
     maxes = {'max_flow_WRRF': max_flow_WRRF, 'max_flow_dstream': max_flow_dstream,
                 'max_TSSLoad_WRRF': max_TSSLoad_WRRF, 'max_TSSLoad_dstream': max_TSSLoad_dstream}
@@ -168,40 +173,62 @@ if noControl == 1:
     print("Done with no control")
 
     if save == 1:
-        fileName = '../data/results/no_control/' + saveNames[0] + '.pkl'
-        with open(fileName,'w') as f:
-            pickle.dump([time,ustream_depths,WRRF_flow,WRRF_TSSLoad,dstream_flows,maxes['max_flow_WRRF'],maxes['max_TSSLoad_WRRF']],f)
+        if saveType == "pickle":
+            fileName = '../data/results/no_control/' + saveNames[0] + '.pkl'
+            with open(fileName,'w') as f:
+                pickle.dump([time,ustream_depths,WRRF_flow,WRRF_TSSLoad,dstream_flows,maxes['max_flow_WRRF'],maxes['max_TSSLoad_WRRF']],f)
+        elif saveType == "numpy":
+            fileName = '../data/results/no_control/' + saveNames[0] + '.npy'
+            dict = {'time':time, 'ustream_depths':ustream_depths, 'WRRF_flow':WRRF_flow, 'WRRF_TSSLoad':WRRF_TSSLoad, 'dstream_flows':dstream_flows, 'max_flow_WRRF':maxes['max_flow_WRRF'], 'max_TSSLoad_WRRF':maxes['max_TSSLoad_WRRF']}
+            np.save(fileName,dict)
         print('No control results saved')
+        del dict
 
+counter = 0
 if control == 1:
     for q in range(0,len(eps_flows)):
         weights['epsilon_flow'] = eps_flows[q]
+        for r in range(0,len(eps_TSS)):
+            weights['epsilon_TSS'] = eps_TSS[r]
 
-        # Runs simulation for MBC case
-        time_state, time_control, ustream_depths, dstream_flows, WRRF_flow, WRRF_TSSLoad, price, demands, gates, setpts_all, ctrlParams = simulation_control(env, n_trunkline, n_ISDs, ctrlParams, sysSpecs, weights, orificeDict, maxes)
+            counter += 1
 
-        # Prints cumulative TSS load at WRRF
-        print('Sum of WRRF_TSSLoad: ' + '%.2f' % sum(WRRF_TSSLoad))
+            # Runs simulation for MBC case
+            time_state, time_control, ustream_depths, dstream_flows, WRRF_flow, WRRF_TSSLoad, price, demands, gates, setpts_all, ctrlParams = simulation_control(env, n_trunkline, n_ISDs, ctrlParams, sysSpecs, weights, orificeDict, maxes, timesteps)
 
-        if plotParams['plot'] == 1:
-            plot_control(n_trunkline, n_ISDs, ctrlParams, plotParams, time_state, time_control, ustream_depths, WRRF_flow, WRRF_TSSLoad, dstream_flows, maxes, setpts_all, price, demands, gates)
+            # Prints cumulative TSS load at WRRF
+            print('Sum of WRRF_TSSLoad: ' + '%.2f' % sum(WRRF_TSSLoad))
 
-        if ctrlParams['objType'] == "flow":
-            print("Done with MBC, Objective: " + ctrlParams['objType'] + ", Setpoint: " + str(ctrlParams['setpt_WRRF_flow']))
-        elif ctrlParams['objType'] == "TSS":
-            print("Done with MBC, Objective: " + ctrlParams['objType'] + ", Setpoint: " + str(ctrlParams['setpt_WRRF_TSS']))
-        elif ctrlParams['objType'] == "both":
-            print("Done with MBC, Objective: " + ctrlParams['objType'] + ", Flow setpoint: " + str(ctrlParams['setpt_WRRF_flow']) + ", TSS setpoint: " + str(ctrlParams['setpt_WRRF_TSS']))
+            if plotParams['plot'] == 1:
+                plot_control(n_trunkline, n_ISDs, ctrlParams, plotParams, time_state, time_control, ustream_depths, WRRF_flow, WRRF_TSSLoad, dstream_flows, maxes, setpts_all, price, demands, gates)
 
-        if save == 1:
-            fileName = '../data/results/control/' + saveNames[0] + '_' + '%02d' % (q+1) + '.pkl'
-            if ctrlParams['objType'] == "both":
-                with open(fileName,'w') as f:
-                    pickle.dump([ISDs,weights,time_state,time_control,ustream_depths,WRRF_flow,ctrlParams['setpt_WRRF_flow'],ctrlParams['setpt_WRRF_TSS'],maxes['max_flow_WRRF'],maxes['max_TSSLoad_WRRF'],WRRF_TSSLoad,dstream_flows,maxes['max_flow_dstream'],demands,price,gates],f)
-            else:
-                with open(fileName,'w') as f:
-                    pickle.dump([ISDs,weights,time_state,time_control,ustream_depths,WRRF_flow,ctrlParams['setpt_WRRF_flow'],ctrlParams['setpt_WRRF_TSS'],maxes['max_flow_WRRF'],maxes['max_TSSLoad_WRRF'],WRRF_TSSLoad,dstream_flows,setpts_all,maxes['max_flow_dstream'],demands,price,gates],f)
-            print('Control results saved')
+            if ctrlParams['objType'] == "flow":
+                print("Done with MBC, Objective: " + ctrlParams['objType'] + ", Setpoint: " + str(ctrlParams['setpt_WRRF_flow']))
+            elif ctrlParams['objType'] == "TSS":
+                print("Done with MBC, Objective: " + ctrlParams['objType'] + ", Setpoint: " + str(ctrlParams['setpt_WRRF_TSS']))
+            elif ctrlParams['objType'] == "both":
+                print("Done with MBC, Objective: " + ctrlParams['objType'] + ", Flow setpoint: " + str(ctrlParams['setpt_WRRF_flow']) + ", TSS setpoint: " + str(ctrlParams['setpt_WRRF_TSS']))
+
+            if save == 1:
+                if saveType == "pickle":
+                    fileName = '../data/results/control/' + saveNames[0] + '_' + '%02d' % (counter) + '.pkl'
+                    if ctrlParams['objType'] == "both":
+                        with open(fileName,'w') as f:
+                            pickle.dump([ISDs,weights,time_state,time_control,ustream_depths,WRRF_flow,ctrlParams['setpt_WRRF_flow'],ctrlParams['setpt_WRRF_TSS'],maxes['max_flow_WRRF'],maxes['max_TSSLoad_WRRF'],WRRF_TSSLoad,dstream_flows,maxes['max_flow_dstream'],demands,price,gates],f)
+                    else:
+                        with open(fileName,'w') as f:
+                            pickle.dump([ISDs,weights,time_state,time_control,ustream_depths,WRRF_flow,ctrlParams['setpt_WRRF_flow'],ctrlParams['setpt_WRRF_TSS'],maxes['max_flow_WRRF'],maxes['max_TSSLoad_WRRF'],WRRF_TSSLoad,dstream_flows,setpts_all,maxes['max_flow_dstream'],demands,price,gates],f)
+                elif saveType == "numpy":
+                    fileName = '../data/results/control/' + saveNames[0] + '_' + '%02d' % (counter) + '.npy'
+                    if ctrlParams['objType'] == "both":
+                        dict = {'ISDs':ISDs, 'weights':weights, 'time_state':time_state, 'time_control':time_control, 'ustream_depths':ustream_depths, 'WRRF_flow':WRRF_flow, 'setpt_WRRF_flow':ctrlParams['setpt_WRRF_flow'], 'setpt_WRRF_TSS':ctrlParams['setpt_WRRF_TSS'], 'max_flow_WRRF':maxes['max_flow_WRRF'], 'max_TSSLoad_WRRF':maxes['max_TSSLoad_WRRF'], 'WRRF_TSSLoad':WRRF_TSSLoad, 'dstream_flows':dstream_flows, 'max_flow_dstream':maxes['max_flow_dstream'], 'demands':demands, 'price':price, 'gates':gates}
+                        np.save(fileName,dict)
+                    else:
+                        dict = {'ISDs':ISDs, 'weights':weights, 'time_state':time_state, 'time_control':time_control, 'ustream_depths':ustream_depths, 'WRRF_flow':WRRF_flow, 'setpt_WRRF_flow':ctrlParams['setpt_WRRF_flow'], 'setpt_WRRF_TSS':ctrlParams['setpt_WRRF_TSS'], 'max_flow_WRRF':maxes['max_flow_WRRF'], 'max_TSSLoad_WRRF':maxes['max_TSSLoad_WRRF'], 'WRRF_TSSLoad':WRRF_TSSLoad, 'dstream_flows':dstream_flows, 'setpts_all':setpts_all, 'max_flow_dstream':maxes['max_flow_dstream'], 'demands':demands, 'price':price, 'gates':gates}
+                        np.save(fileName,dict)
+                print('Control results saved')
+                del dict
 
 if plotParams['plot'] == 1:
     plot_finish(plotParams['normalize'])
+    plt.show()
