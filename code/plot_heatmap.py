58f5dd3e556176b79014023481b957674846af28
stroutm <stroutm@umich.edu>
2019-03-25 12:11:52 -0400

    updated plotting of heatmaps

--- code/plot_heatmap.py
+++ code/plot_heatmap.py
@@ -9,8 +9,8 @@ import scipy.io as sio
 dataLoad = sio.loadmat('../data/results/control/resultsSumm_201701.mat')
 eps_flow = dataLoad['eps_flow'][0]
 eps_tss = dataLoad['eps_tss'][0]
-flow_var = dataLoad['flow_var']
-tss_var = dataLoad['tss_var']
+flow_var = dataLoad['flow_var_dw']
+tss_var = dataLoad['tss_var_dw']
 flow_peak = dataLoad['flow_peak']
 tss_peak = dataLoad['tss_peak']
 flow_peak_red = dataLoad['flow_peak_red']
@@ -28,7 +28,43 @@ timeFlood = dataLoad['timeFlood']
 #ax1.set_title("Flow Variance")
 #fig1.tight_layout()
 
-def heatmap(data, row_labels, col_labels, xLabel, yLabel, ax=None,
+def heatmap2(data, threshold, col_labels, row_labels, xLabel, yLabel, ax=None,
+             cbar_kw={}, cbarlabel="", **kwargs):
+    
+    im = ax.imshow(data, vmax=threshold, **kwargs)
+    cbar = ax.figure.colorbar(im, ax=ax, extend='max', **cbar_kw)
+    cbar.cmap.set_over('white')
+    ax.set_title(cbarlabel)
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+    ax.set_xlabel(xLabel)
+    ax.set_ylabel(yLabel)
+
+    # Let the horizontal axes labeling appear on top.
+    #ax.tick_params(top=True, bottom=False,
+    #               labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    #plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+    #         rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+    
+    return im, cbar
+
+def heatmap(data, col_labels, row_labels, xLabel, yLabel, ax=None,
             cbar_kw={}, cbarlabel="", **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
@@ -90,23 +126,25 @@ def heatmap(data, row_labels, col_labels, xLabel, yLabel, ax=None,
 
 cmaps = ["viridis","magma","inferno","YlGnBu_r"]
 cmapsNo = 2
+
+
 fig1, ax1 = plt.subplots()
-im1, cbar1 = heatmap(flow_var, eps_tss, eps_flow, "eps_flow", "eps_TSS", ax=ax1,
-                   cmap=cmaps[cmapsNo], cbarlabel="Flow Variance")
+im1, cbar1 = heatmap2(flow_var, 1000, eps_tss, eps_flow, "eps_TSS", "eps_flow", ax=ax1,
+                   cmap=cmaps[cmapsNo], cbarlabel="Dry-Weather Flow Variance")
 fig2, ax2 = plt.subplots()
-im2, cbar2 = heatmap(tss_var, eps_tss, eps_flow, "eps_flow", "eps_TSS", ax=ax2,
-                   cmap=cmaps[cmapsNo], cbarlabel="TSS Variance")
+im2, cbar2 = heatmap2(tss_var, 0.1, eps_tss, eps_flow, "eps_TSS", "eps_flow", ax=ax2,
+                   cmap=cmaps[cmapsNo], cbarlabel="Dry-Weather TSS Load Variance")
 fig3, ax3 = plt.subplots()
-im3, cbar3 = heatmap(flow_peak, eps_tss, eps_flow, "eps_flow", "eps_TSS", ax=ax3,
+im3, cbar3 = heatmap(flow_peak, eps_tss, eps_flow, "eps_TSS", "eps_flow", ax=ax3,
                    cmap=cmaps[cmapsNo], cbarlabel="Control/No Control Flow Peak")
 fig4, ax4 = plt.subplots()
-im4, cbar4 = heatmap(tss_peak, eps_tss, eps_flow, "eps_flow", "eps_TSS", ax=ax4,
+im4, cbar4 = heatmap(tss_peak, eps_tss, eps_flow, "eps_TSS", "eps_flow", ax=ax4,
                    cmap=cmaps[cmapsNo], cbarlabel="Control/No Control TSS Load Peak")
 fig5, ax5 = plt.subplots()
-im5, cbar5 = heatmap(tss_load_remaining, eps_tss, eps_flow, "eps_flow", "eps_TSS", ax=ax5,
-                   cmap=cmaps[cmapsNo], cbarlabel="Percent of TSS Load Remaining in Sewer")
+im5, cbar5 = heatmap(tss_load_remaining, eps_tss, eps_flow, "eps_TSS", "eps_flow", ax=ax5,
+                   cmap=cmaps[cmapsNo], cbarlabel="Fraction of TSS Load Remaining in Sewer")
 fig6, ax6 = plt.subplots()
-im6, cbar6 = heatmap(timeFlood, eps_tss, eps_flow, "eps_flow", "eps_TSS", ax=ax6,
+im6, cbar6 = heatmap(timeFlood, eps_tss, eps_flow, "eps_TSS", "eps_flow", ax=ax6,
                    cmap=cmaps[cmapsNo], cbarlabel="Timesteps where a Storage is Full")
 
 fig1.tight_layout()
